321e09066e077142584562ae6921099aa8aa6e14
Fab Burst <fabimacg5@gmail.com>
2018-11-06 17:50:18 +0100

    Articles fully functional

--- app/Table/Article.php
+++ app/Table/Article.php
@@ -3,13 +3,18 @@ namespace App\Table;
 
 class Article {
 
-    public function getURL(){
+    public function __get($key){
+      $method = 'get'. ucfirst($key);
+      $this->$key = $this->$method();
+      return $this->$key;
+    }
+    public function getUrl(){
         return 'index.php?p=article&id=' . $this->id;
     }
 
     public function getExtrait(){
         $html = '<p>'. substr($this->content, 0, 200) .'</p>';
-        $html .= '<p><a href=" . $this->getURL() . ">Voir la suite</a></p>';
+        $html .= '<p><a href="' . $this->getUrl() . '">Voir la suite</a></p>';
         return $html;
     }
 }
--- pages/single.php
+++ pages/single.php
@@ -1,1 +1,9 @@
-<h2>Je suis la single</h2>
+<?php
+
+$post = $db->prepare('SELECT * FROM articles WHERE id = ?',
+    [$_GET['id']],'App\Table\Article', true);
+
+?>
+<h1><?= $post->title; ?></h1>
+
+<p><?= $post->content; ?></p>
